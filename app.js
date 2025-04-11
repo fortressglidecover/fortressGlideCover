fa25bafa0f55dbd1722bbd310a118db7ecba99f2
fortressglidecover <fortressglidecover@gmail.com>
2025-04-11 16:44:41 -0400

    First commit for FortressGlideCover

---
+++ app.js
@@ -0,0 +1,15 @@
+
+
+
+
+function openWindowSizePopUp(){
+    console.log("Open Popup");
+    document.getElementById("WindowSizePopUpId").style.display = "block";
+}
+
+function closeWindowSizePopUp() {
+    console.log("Close Popup");
+    document.getElementById("WindowSizePopUpId").style.display = "none";
+}
+
+
