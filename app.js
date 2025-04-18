01a6e4ccf608dca0602fa01b7fcc0b30ce83dbf4
fortressglidecover <fortressglidecover@gmail.com>
2025-04-18 15:43:17 -0400

    updated text images and layout

--- app.js
+++ app.js
@@ -12,4 +12,11 @@ function closeWindowSizePopUp() {
     document.getElementById("WindowSizePopUpId").style.display = "none";
 }
 
-
+function showLinkFullVideo() {
+    console.log("Open Video Link");
+    document.getElementById("showLinkFullVideoID").style.display = "block";
+}
+function closeShowLinkFullVideo() {
+    console.log("Close Popup");
+    document.getElementById("showLinkFullVideoID").style.display = "none";
+}
